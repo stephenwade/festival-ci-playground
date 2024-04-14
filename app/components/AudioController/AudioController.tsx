e115be08256db2a21b9ea05349f1dffe46f43bfa
Stephen Wade <stephen@stephenwade.me>
2024-04-14 00:46:41 -0400

    Test

--- app/components/AudioController/AudioController.tsx
+++ app/components/AudioController/AudioController.tsx
@@ -19,7 +19,6 @@ interface AudioControllerProps {
 
   children: (args: {
     showInfo: ShowInfo;
-
     initializeAudio: () => Promise<void>;
   }) => ReactNode;
 }
--- playwright/ct-tests/AudioController/AudioControllerTest.tsx
+++ playwright/ct-tests/AudioController/AudioControllerTest.tsx
@@ -3,47 +3,35 @@ import { useLoaderData } from '@remix-run/react';
 import { createRemixStub } from '@remix-run/testing';
 import { addSeconds } from 'date-fns';
 import type { FC } from 'react';
-import { useDeepCompareMemo } from 'use-deep-compare';
 
 import { AudioController } from '~/components/AudioController';
 import { useShowInfo } from '~/hooks/useShowInfo';
-import type { ShowData } from '~/types/ShowData';
 
-interface GetMockDataProps {
-  offsetSec?: number;
-}
-
-function getMockData({
-  offsetSec = -5,
-}: GetMockDataProps): Pick<ShowData, 'serverDate' | 'sets'> {
-  const now = new Date();
-
-  const sets: ShowData['sets'] = [
-    {
-      id: 'ccc19b72-4a68-3219-9409-ef1ef0d75643',
-      audioUrl:
-        'https://festivalci.z13.web.core.windows.net/90-sec-silence.mp3?1',
-      artist: 'Artist 1',
-      start: addSeconds(now, 0 - offsetSec).toISOString(),
-      duration: 90,
-    },
-  ];
-
-  return {
-    serverDate: now.toISOString(),
-    sets,
-  };
-}
-
-interface TestProps extends GetMockDataProps {
+interface TestProps {
   forceSkipAudioContext: boolean;
 }
 
 function AudioControllerDisplay() {
-  const { forceSkipAudioContext, ...props } = useLoaderData<TestProps>();
+  const { forceSkipAudioContext } = useLoaderData<TestProps>();
 
-  const data = useDeepCompareMemo(() => getMockData(props), [props]);
-  const { targetShowInfo } = useShowInfo(data, { ci: true });
+  const now = new Date();
+
+  const { targetShowInfo } = useShowInfo(
+    {
+      serverDate: now.toISOString(),
+      sets: [
+        {
+          id: 'ccc19b72-4a68-3219-9409-ef1ef0d75643',
+          audioUrl:
+            'https://festivalci.z13.web.core.windows.net/90-sec-silence.mp3?1',
+          artist: 'Artist 1',
+          start: addSeconds(now, 5).toISOString(),
+          duration: 90,
+        },
+      ],
+    },
+    { ci: true },
+  );
 
   return (
     <AudioController
@@ -52,11 +40,9 @@ function AudioControllerDisplay() {
     >
       {({ showInfo, initializeAudio }) => (
         <div>
-          <p>
-            <button data-testid="init-button" onClick={initializeAudio}>
-              Initialize audio
-            </button>
-          </p>
+          <button data-testid="init-button" onClick={initializeAudio}>
+            Initialize audio
+          </button>
           <p>Show status: {showInfo.status}</p>
         </div>
       )}
